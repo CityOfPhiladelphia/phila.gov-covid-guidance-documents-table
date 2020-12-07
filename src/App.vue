a265bb1e7a7f70b131c23b69656e5b0be5cee088
m-atia <mohamed.atia@phila.gov>
2020-12-07 10:57:49 -0500

    commiting site table

---
+++ src/App.vue
@@ -0,0 +1,107 @@
+<template>
+  <div id="vue-app">
+    <div
+      data-swiftype-index="true"
+      class="entry-content"
+    >
+      <!-- Site finder -->
+      <section class="mtl">
+        <div class="large-24 columns">
+          <SiteFinder
+            :document-table="documentTables"
+            :query="query"
+            title-hash="Guidance documents"
+          />
+        </div>
+      </section>
+      <!-- Site finder -->
+    </div> <!-- End .entry-content -->
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import axios from 'axios';
+import SiteFinder from './components/SiteFinder.vue';
+import { ClientTable } from 'vue-tables-2';
+// import { _ } from 'vue-underscore';
+const app_key = "keyNcseMILLKVVFC3";
+
+Vue.use(ClientTable, {
+  options: {}, 
+  useVuex: false,
+  theme: 'bootstrap3',
+  template: 'default',
+});
+
+export default {
+  name: 'App',
+  components: {
+    SiteFinder,
+  },
+  data () {
+    return {
+      api: {
+        getDocumentTables: 'https://staging-admin.phila.gov/wp-json/documents/v1/document',
+      },
+      documentTables: [],
+      query: null,
+    };
+  },
+  created () {
+    let vm = this;
+    vm.init();
+  },
+  methods: {
+    getDocumentTables () {
+      let vm = this;
+      let slug = window.location.pathname;
+      slug = 'https://api.airtable.com/v0/app6HWoBlToNp1uKQ/Table%201?&view=Grid%20view';
+      return axios.get(
+        slug,
+        { headers: { Authorization: "Bearer " + app_key } },
+      ).
+      then(async (response) => {
+        
+        let tempData = response.data.records.map(function(entry) {
+            return entry["fields"];
+          });
+
+        tempData.forEach(function(site) {
+          site['Title'] = site['English title'];
+          delete site['English title'];
+          site['Format'] = site['Type'];
+          delete site['Type'];
+        });
+
+         
+        vm.documentTables = tempData;
+        return true;
+      }).catch(() => {
+        return false;
+      });
+    },
+    async parseQuery () {
+      let vm = this;
+      vm.query = vm.$route.hash;
+    },
+    async init () {
+      let vm = this;
+      await vm.parseQuery();
+      await vm.getDocumentTables();
+      await vm.scrollToDiv(); 
+    },
+    async scrollToDiv() {
+      let vm = this;
+      let div = vm.$route.hash.split('#')[1];
+      if ( div ) {
+        let distance = document.getElementById(div).offsetTop;
+        window.scrollTo({
+          top: distance,
+          behavior: 'smooth',
+        });
+      }
+    },
+  },
+};
+</script>
---
+++ src/components/SiteFinder.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div>
+      <v-client-table
+        :ref="titleId"
+        :name="titleId"
+        :data="documentTable"
+        :columns="columns"
+        :options="options"
+      >
+        <a
+          :id="row.id"
+          slot="title"
+          slot-scope="{row}"
+          :href="row['English URL']"
+        ><span class="title">{{ row.title }}</span></a>
+        <a
+          slot="format"
+          slot-scope="{row}"
+          class="text-uppercase"
+          :href="row['English URL']"
+        >{{ row.format }} <i class="fas fa-download fa-fw" /></a>
+        <div slot="afterFilter">
+          <span><i class="fas fa-search fa-2x" /></span>
+        </div>
+      </v-client-table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SiteFinder',
+  props: {
+    documentTable: {
+      type: Array,
+      default: () => {
+        return [];
+      },
+    },
+    query: {
+      type: String,
+      default: () => {
+        return null;
+      },
+    },
+    titleHash: {
+      type: String,
+      default: () => {
+        return null;
+      },
+    },
+  },
+  data: function() {
+    return {
+      columns: [ 'Title', 'Category', 'Format'],
+      // column_titles: ['Access Center', 'Address', 'ZIP Code'],
+      options: {
+        texts: {
+          filterPlaceholder: "Begin typing to filter by title or category",
+        },
+        perPage: 400,
+        perPageValues: [],
+      },
+      titleId: this.titleHash.split(' ').join('-').toLowerCase(),
+      title: "Guidance documents",
+    };
+  },
+};
+</script>
