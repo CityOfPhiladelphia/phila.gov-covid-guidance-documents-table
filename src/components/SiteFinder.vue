db4fb76d7b658b95c9faab6771dc376e31f27078
m-atia <mohamed.atia@phila.gov>
2020-12-09 09:12:17 -0500

    remove pubilc path for embedding

--- src/components/SiteFinder.vue
+++ src/components/SiteFinder.vue
@@ -19,7 +19,7 @@
             v-model="multiselectValue"
             :options="categoryList"
             placeholder="Showing all categories"
-            @input="filterTable"
+     
           />
         <div slot="afterFilter">
           <span><i class="fas fa-search fa-2x" /></span>
@@ -30,8 +30,14 @@
 </template>
 
 <script>
+import Multiselect from 'vue-multiselect';
+// import { Event } from 'vue-tables-2';
+
 export default {
   name: 'SiteFinder',
+  components: {
+    Multiselect,
+  },
   props: {
     documentTable: {
       type: Array,
@@ -52,9 +58,81 @@ export default {
       },
     },
   },
+  mounted () {
+    // let vm = this;
+    // vm.init();
+  },
+  methods: {
+    // onRowClick(event) {
+    //   window.location.href = event.row.url;
+    // },
+    // async init () {
+    //   let vm = this;
+    //   await vm.searchQuery();
+    //   await vm.populateCategoryList();
+    // },
+    // async searchQuery () {
+    //   let vm = this;
+    //   if(vm.$route.query.table && vm.$route.query.q ) {
+    //     let table = vm.$route.query.table;
+    //     vm.searchTerm = vm.$route.query.q.split('-').join(' ');
+    //     if( table == vm.titleId) {
+    //       vm.showMessage();
+    //       vm.$refs[vm.titleId].setFilter(vm.searchTerm);
+    //     }
+    //   }
+    // },
+    // async populateCategoryList () {
+    //   let vm = this;
+    //   let description = false;
+    //   let tempCategoryList = vm.documentTable.map(doc => {
+    //     if(doc.description) {
+    //       description = true;
+    //     }
+    //     return doc.Category || 'All categories';
+    //   });
+    //   let uniqueCategories = Array.from(new Set(tempCategoryList));
+    //   if(uniqueCategories.length > 1) {
+    //     tempCategoryList.push('All categories');
+    //   } else {
+    //     tempCategoryList = [];
+    //   }
+    //   if (vm.documentTable.labelColumnTitle) {
+    //     vm.columns.push(vm.documentTable.labelColumnTitle);
+    //   }
+    //   vm.columns.push('title');
+    //   if (description) {
+    //     vm.columns.push('description');
+    //   }
+    //   if (tempCategoryList.length) {
+    //     vm.columns.push('Category');
+    //   }
+    //   vm.columns.push('Issued or revised date');
+    //   vm.categoryList = [ ...new Set(tempCategoryList) ];
+    // },
+    // filterTable () {
+    //   let vm  = this;
+    //   if (vm.multiselectValue == "All categories") {
+    //     Event.$emit(`vue-tables.${vm.titleId}.filter::dropdownFilter`, null);
+    //   } else {
+    //     Event.$emit(`vue-tables.${vm.titleId}.filter::dropdownFilter`, vm.multiselectValue);
+    //   }
+    // },
+    // closeMessage () {
+    //   let vm  = this;
+    //   vm.viewMessage = false;
+    //   vm.$refs[vm.titleId].resetQuery();
+    // },
+    // showMessage () {
+    //   let vm  = this;
+    //   vm.viewMessage = true;
+    // },
+  },
   data: function() {
     return {
-      columns: [ 'Category', 'Title', 'Format'],
+      categoryList: ['Category', 'Title'],
+      multiselectValue: "",
+      columns: [ 'Title', 'Category',  'Format'],
       // column_titles: ['Access Center', 'Address', 'ZIP Code'],
       options: {
         texts: {
@@ -69,3 +147,4 @@ export default {
   },
 };
 </script>
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
