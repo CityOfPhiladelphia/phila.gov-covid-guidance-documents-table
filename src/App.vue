39255375a19fe3fd1d2936d284be2c3fe17715aa
Derrick Dieso <derrickjdieso@gmail.com>
2021-03-09 15:10:11 -0500

    working without dedicated files

--- src/App.vue
+++ src/App.vue
@@ -78,8 +78,8 @@ export default {
   },
   data () {
     return {
-      // postId: window.phila_js_vars.postID,
-      postId: 167190,
+      postId: window.phila_js_vars.postID,
+      // postId: 167190,
       api: {
         url: process.env.VUE_APP_API_URL,
         endpoint: 'wp-json/programs/v1/archives/',
